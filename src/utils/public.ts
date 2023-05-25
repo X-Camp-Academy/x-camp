d55372688b043f0450cee181b07f778314d585b6
= <=>
2023-05-25 17:28:11 +0800

    初始化配置x-camp

---
+++ src/utils/public.ts
@@ -0,0 +1,16 @@
+/**
+ * 选择语言
+ * @param lang 语言
+ * @param zhText 中文字段
+ * @param enText 英文字段
+ * @returns string
+ */
+export const getTransResult = (
+  lang: 'zh' | 'en',
+  zhText: string | undefined,
+  enText: string | undefined
+) => {
+  if (zhText === undefined && enText === undefined) return '';
+  if (lang == 'zh') return zhText ? zhText : enText;
+  else return enText ? enText : zhText;
+};
