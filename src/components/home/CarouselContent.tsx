d3569067a1cf1674dd8e65fdd04ac8d6c0c914e5
healthyhcc <healthyhcc@gmail.com>
2023-06-16 18:13:36 +0800

    修复首页轮播图铺满

--- src/components/home/CarouselContent.tsx
+++ src/components/home/CarouselContent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React from "react";
 import {
   Carousel,
   Space,
@@ -9,13 +9,9 @@ import {
   Form,
   Input,
   Typography,
-  Row,
-  Col,
-  ConfigProvider,
 } from "antd";
 import { useMobile } from "@/utils";
 import styles from "./CarouselContent.module.scss";
-import { CarouselRef } from "antd/es/carousel";
 
 const { Title, Paragraph, Text } = Typography;
 
@@ -33,105 +29,101 @@ const CarouselContent: React.FC = () => {
   return (
     <div className={styles.bannerContainer}>
       <div className={`${styles.banner} container`}>
-        <Row justify="space-between" align="middle">
-          <Col xs={24} sm={24} md={16}>
-            <Carousel
-              autoplay={false}
-              dots={{ className: styles.carouselDots }}
-            >
-              {carouselImages.map((item) => (
-                <Image
-                  key={item}
-                  alt="carousel"
-                  preview={false}
-                  src={item}
-                  height={isMobile ? 300 : 432}
-                  className={styles.carouselImage}
-                />
-              ))}
-            </Carousel>
-          </Col>
+        <Carousel autoplay={false} dots={{ className: styles.carouselDots }}>
+          <Image
+            alt="carousel"
+            preview={false}
+            width={"100%"}
+            height={576}
+            src="/image/home/carousel-bg.png"
+          />
+          <Image
+            alt="carousel"
+            preview={false}
+            width={"100%"}
+            height={576}
+            src="/image/home/carousel-bg.png"
+          />
+        </Carousel>
+        <div className={styles.cardFrom}>
+          <Card
+            title="Free Consultation"
+            extra={
+              <Image
+                src="/image/home/think.png"
+                alt="logo"
+                preview={false}
+                width={60}
+                height={50}
+                style={{ marginBottom: -8 }}
+              />
+            }
+            headStyle={{
+              color: "#fff",
+              fontSize: 20,
+              fontWeight: "normal",
+              height: 64,
+              lineHeight: 36,
+              backgroundColor: "#FFAD11",
+            }}
+            bodyStyle={{
+              paddingTop: 36,
+              paddingBottom: 16,
+            }}
+            className={styles.card}
+          >
+            <Form name="basic" onFinish={onFinish} className={styles.form}>
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your username!",
+                  },
+                ]}
+              >
+                <Input placeholder="孩子昵称" />
+              </Form.Item>
 
-          <Col xs={24} sm={24} md={{ span: 7, offset: 1 }}>
-            <Card
-              title="Free Consultation"
-              extra={
-                <Image
-                  src="/image/home/think.png"
-                  alt="logo"
-                  preview={false}
-                  width={60}
-                  height={50}
-                  style={{ marginBottom: -8 }}
-                />
-              }
-              headStyle={{
-                color: "#fff",
-                fontSize: 20,
-                fontWeight: "normal",
-                height: 64,
-                lineHeight: 36,
-                backgroundColor: "#FFAD11",
-              }}
-              bodyStyle={{
-                paddingTop: 36,
-                paddingBottom: 16,
-              }}
-              className={styles.card}
-            >
-              <Form name="basic" onFinish={onFinish} className={styles.form}>
-                <Form.Item
-                  name="username"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your username!",
-                    },
-                  ]}
+              <Form.Item
+                name="grade"
+                rules={[
+                  { required: true, message: "Please input your grade!" },
+                ]}
+              >
+                <Input placeholder="孩子年级" />
+              </Form.Item>
+
+              <Form.Item
+                name="email"
+                rules={[
+                  { required: true, message: "Please input your email!" },
+                ]}
+              >
+                <Input type="email" placeholder="E-mail" />
+              </Form.Item>
+
+              <Form.Item
+                name="number"
+                rules={[
+                  { required: true, message: "Please input your number!" },
+                ]}
+              >
+                <Input placeholder="wechat ID/Phone Number" />
+              </Form.Item>
+
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className={styles.submit}
                 >
-                  <Input placeholder="孩子昵称" />
-                </Form.Item>
-
-                <Form.Item
-                  name="grade"
-                  rules={[
-                    { required: true, message: "Please input your grade!" },
-                  ]}
-                >
-                  <Input placeholder="孩子年级" />
-                </Form.Item>
-
-                <Form.Item
-                  name="email"
-                  rules={[
-                    { required: true, message: "Please input your email!" },
-                  ]}
-                >
-                  <Input type="email" placeholder="E-mail" />
-                </Form.Item>
-
-                <Form.Item
-                  name="number"
-                  rules={[
-                    { required: true, message: "Please input your number!" },
-                  ]}
-                >
-                  <Input placeholder="wechat ID/Phone Number" />
-                </Form.Item>
-
-                <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    className={styles.submit}
-                  >
-                    免费预约公开课（每周二）
-                  </Button>
-                </Form.Item>
-              </Form>
-            </Card>
-          </Col>
-        </Row>
+                  免费预约公开课（每周二）
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </div>
       </div>
     </div>
   );
