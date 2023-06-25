7907b94ff184c6da1e1a870eb70cf8812511d3b5
healthyhcc <healthyhcc@gmail.com>
2023-06-25 17:57:34 +0800

    完成about-us x-alumni

--- src/components/about-us/x-alumni/UpcomingEvents.tsx
+++ src/components/about-us/x-alumni/UpcomingEvents.tsx
@@ -59,6 +59,39 @@ const UpcomingEvents: React.FC = () => {
             </Col>
           ))}
         </Row>
+        <Row gutter={32} className={styles.row}>
+          {[1, 2, 3].map((item, index) => (
+            <Col key={index} xs={24} sm={24} md={8}>
+              <ColorfulCard border="bottom" index={index}>
+                <Card
+                >
+                  <Space direction="vertical">
+                    <Text className={styles.cardMonth}>JUN</Text>
+                    <Text className={styles.cardDay}>10</Text>
+                    <Paragraph className={styles.cardParagraph}>
+                      USACO Director Brian Dean's Q&A Session - Webinar
+                      Recording
+                    </Paragraph>
+                    <Space direction="vertical">
+                      <Text className={styles.cardText}>
+                        <HistoryOutlined className={styles.cardIcon} />
+                        Tuesday, June 13, 2023 | 6:00 PM - 7:00 PM PDT
+                      </Text>
+                      <Text className={styles.cardText}>
+                        <LaptopOutlined className={styles.cardIcon} />
+                        Zoom
+                      </Text>
+                      <Text className={styles.cardText}>
+                        <UserOutlined className={styles.cardIcon} />
+                        Organizer | X-Camp{" "}
+                      </Text>
+                    </Space>
+                  </Space>
+                </Card>
+              </ColorfulCard>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
