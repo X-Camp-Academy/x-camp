787b1800b3d98473a75dbd80d7d6b56978ca4854
zhangjiajun <183728895@qq.com>
2023-06-26 14:42:49 +0800

    拉取组件

--- src/components/courses/camps/index.tsx
+++ src/components/courses/camps/index.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { ConfigProvider, Layout } from 'antd';
+import { ConfigProvider, Layout, Space } from 'antd';
 import React from 'react';
 import styles from './index.module.scss';
 import TopBanner from './top-banner';
 import CampIntro from './camp-intro';
 import CampCarousel from './camp-carousel';
+import CourseAbstract from '../detail/top-banner/course-card/course-abstract';
+import ColorfulCard from '@/components/common/colorful-card';
 const { Content } = Layout;
 const CourseCamps = () => {
   return (
@@ -20,6 +22,13 @@ const CourseCamps = () => {
           <TopBanner />
           <CampIntro />
           <CampCarousel />
+          <div className={styles.courseCard}>
+            <div className="container">
+              <ColorfulCard border={'bottom'} index={1} animate={false}>
+                <CourseAbstract />
+              </ColorfulCard>
+            </div>
+          </div>
         </Content>
       </Layout>
     </ConfigProvider>
