54a7218523c58e612d8f5af1267ae62a78db590b
chenliangliang <llight105#163.com>
2023-06-26 18:00:34 +0800

    weekly-open-house

--- src/components/resources/weekly-open-house/index.tsx
+++ src/components/resources/weekly-open-house/index.tsx
@@ -1,8 +1,12 @@
 "use client";
-import { ConfigProvider, Layout } from "antd";
+import { ConfigProvider, Layout, Space } from "antd";
 import React from "react";
 import styles from "./index.module.scss";
 import TopBanner from "./top-banner";
+import Comments from "@/components/home/Comments";
+import JoinWay from "./join-way";
+import Introduction from "./introduction";
+import AppointmentCard from "./appointment-card";
 const { Content } = Layout;
 
 const WeeklyOpenHouse = () => {
@@ -17,7 +21,12 @@ const WeeklyOpenHouse = () => {
       <Layout className={styles.main}>
         <Content>
           <TopBanner />
-          <div>123</div>
+          <Space direction="vertical" size={100} className={styles.content}>
+            <JoinWay />
+            <Introduction />
+            <AppointmentCard />
+          </Space>
+          <Comments />
         </Content>
       </Layout>
     </ConfigProvider>
