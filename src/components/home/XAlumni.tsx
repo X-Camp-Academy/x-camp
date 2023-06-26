d1d2ca47fc092c37d01b529563ff1f11b892491b
healthyhcc <healthyhcc@gmail.com>
2023-06-26 10:41:12 +0800

    fix home

--- src/components/home/XAlumni.tsx
+++ src/components/home/XAlumni.tsx
@@ -37,6 +37,12 @@ const XAlumni: React.FC = () => {
       description:
         "Ryan is a computer science student and artificial intelligence researcher in Massachusetts. He has been programming for more than 10 years in a variety of programming languages including Clojure, Java, C++ and Python. With experience in software development and competitive programming, Ryan loves guiding people in understanding new concepts for themselves with a wide range of uses.",
     },
+    {
+      avatar: "/image/home/alumni-ryan.png",
+      name: "Ryans",
+      description:
+        "Ryan is a computer science student and artificial intelligence researcher in Massachusetts. He has been programming for more than 10 years in a variety of programming languages including Clojure, Java, C++ and Python. With experience in software development and competitive programming, Ryan loves guiding people in understanding new concepts for themselves with a wide range of uses.",
+    },
   ];
   const carouselRef = useRef<CarouselRef>(null);
 
@@ -47,6 +53,7 @@ const XAlumni: React.FC = () => {
     carouselRef?.current?.next();
   };
   const isMobile = useMobile();
+
   return (
     <div className={styles.xalumniContainer}>
       <div className={`${styles.xalumni} container`}>
@@ -67,80 +74,61 @@ const XAlumni: React.FC = () => {
         >
           <LeftOutlined />
         </Button>
-        <Carousel ref={carouselRef} dots={{ className: styles.carouselDots }}>
-          <div>
-            <Row
-              gutter={40}
-              justify="center"
-              align="middle"
-              className={styles.row}
-            >
-              {alumniData.map((item, index) => {
-                return (
-                  <Col
-                    key={index}
-                    xs={24}
-                    sm={24}
-                    md={24}
-                    lg={8}
-                    className={styles.col}
-                  >
-                    <ColorfulCard border="top" index={index} animate={false}>
-                      <Card>
-                        <Space direction="vertical" align="center">
-                          <Avatar src={item.avatar} className={styles.avatar} />
-                          <Text className={styles.cardTitle}>{item?.name}</Text>
-                          <Paragraph
-                            ellipsis={isMobile ? { rows: 3 } : { rows: 5 }}
-                            className={styles.cardParagraph}
-                          >
-                            {item?.description}
-                          </Paragraph>
-                        </Space>
-                      </Card>
-                    </ColorfulCard>
-                  </Col>
-                );
-              })}
-            </Row>
-          </div>
-          <div>
-            <Row
-              gutter={40}
-              justify="center"
-              align="middle"
-              className={styles.row}
-            >
-              {alumniData.map((item, index) => {
-                return (
-                  <Col
-                    key={index}
-                    xs={24}
-                    sm={24}
-                    md={24}
-                    lg={8}
-                    className={styles.col}
-                  >
-                    <ColorfulCard border="top" index={index} animate={false}>
-                      <Card>
-                        <Space direction="vertical">
-                          <Avatar src={item.avatar} className={styles.avatar} />
-                          <Text className={styles.cardTitle}>{item?.name}</Text>
-                          <Paragraph
-                            ellipsis={{ rows: 5 }}
-                            className={styles.cardParagraph}
-                          >
-                            {item?.description}
-                          </Paragraph>
-                        </Space>
-                      </Card>
-                    </ColorfulCard>
-                  </Col>
-                );
-              })}
-            </Row>
-          </div>
+
+        <Carousel
+          ref={carouselRef}
+          slidesToShow={3}
+          slidesToScroll={1}
+          swipeToSlide={true}
+          infinite={false}
+          responsive={[
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 3,
+              },
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+              },
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 1,
+              },
+            },
+          ]}
+          dots={{ className: styles.carouselDots }}
+        >
+          {alumniData.map((item, index) => {
+            return (
+              <ColorfulCard
+                key={index}
+                border="top"
+                index={index}
+                animate={false}
+                className={styles.card}
+              >
+                <Card>
+                  <Space direction="vertical" align="center">
+                    <Avatar src={item.avatar} className={styles.avatar} />
+                    <Text className={styles.cardTitle}>{item?.name}</Text>
+                    <Paragraph
+                      ellipsis={isMobile ? { rows: 3 } : { rows: 5 }}
+                      className={styles.cardParagraph}
+                    >
+                      {item?.description}
+                    </Paragraph>
+                  </Space>
+                </Card>
+              </ColorfulCard>
+            );
+          })}
         </Carousel>
+
         <Button
           type="primary"
           shape="circle"
