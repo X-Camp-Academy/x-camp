5a598a14523d1b09eae960e94571e51cb43433f4
healthyhcc <healthyhcc@gmail.com>
2023-06-27 09:34:31 +0800

    修改首页hover

--- src/components/common/back-top/index.tsx
+++ src/components/common/back-top/index.tsx
@@ -6,23 +6,17 @@ import styles from "./index.module.scss";
 
 const BackTop: React.FC = () => {
   const scroll = useScroll(document);
-  console.log(scroll);
-
   const scrollToTop = () => {
-    // 获取根元素
     const rootElement = document.documentElement || document.body;
-
-    // 滚动根元素到顶部
     rootElement.scrollIntoView({ behavior: "smooth", block: "start" });
   };
 
-  return (
-    scroll &&
-    scroll?.top > 500 && (
-      <div className={styles.backTop} onClick={scrollToTop}>
-        <VerticalAlignTopOutlined className={styles.icon} />
-      </div>
-    )
+  return scroll && scroll?.top > 500 ? (
+    <div className={styles.backTop} onClick={scrollToTop}>
+      <VerticalAlignTopOutlined className={styles.icon} />
+    </div>
+  ) : (
+    <></>
   );
 };
 
