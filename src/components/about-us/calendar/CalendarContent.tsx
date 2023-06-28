2248611d15ba04c841698797b35e99e3afe34ca8
Tangent <605738695@qq.com>
2023-06-28 17:55:47 +0800

    update school calendar

--- src/components/about-us/calendar/CalendarContent.tsx
+++ src/components/about-us/calendar/CalendarContent.tsx
@@ -15,23 +15,25 @@ import {
   RightCircleTwoTone,
   setTwoToneColor,
 } from "@ant-design/icons";
-import styles from "./USACOWinners.module.scss";
 import React, { useRef } from "react";
 import Link from "next/link";
 import ColorfulCard from "@/components/common/colorful-card";
 const { Title, Paragraph, Text } = Typography;
+import styles from "./CalendarContent.module.scss";
 
-const USACOMedal: React.FC = () => {
+const CalendarContent: React.FC = () => {
   return (
     <>
-      <div className={styles.USACOMedalContainer}>
-        511 5313
-        <div className={styles.listContainer}>
-          adfa 32as1df 32as1df3 5as4d1f
+      <div className={styles.calendarContent}>
+        <div className={`${styles.calendarContainer} container`}>
+          <Title className={styles.title}>2023 X-Camp Calendar</Title>
+          <div className={styles.listContainer}>
+            adfa 32as1df 32as1df3 5as4d1f
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default USACOMedal;
+export default CalendarContent;
