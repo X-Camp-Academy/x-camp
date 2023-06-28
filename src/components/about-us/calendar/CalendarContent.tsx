beccad9c65f763c95a43458f7eb555007daca003
Tangent <605738695@qq.com>
2023-06-28 19:42:17 +0800

    finish school calendar

--- src/components/about-us/calendar/CalendarContent.tsx
+++ src/components/about-us/calendar/CalendarContent.tsx
@@ -1,34 +1,125 @@
-import {
-  Space,
-  Row,
-  Col,
-  Card,
-  Image,
-  Typography,
-  Carousel,
-  Button,
-  List,
-} from "antd";
-import { CarouselRef } from "antd/es/carousel";
-import {
-  LeftCircleTwoTone,
-  RightCircleTwoTone,
-  setTwoToneColor,
-} from "@ant-design/icons";
-import React, { useRef } from "react";
-import Link from "next/link";
-import ColorfulCard from "@/components/common/colorful-card";
-const { Title, Paragraph, Text } = Typography;
+import { Button, Timeline, Typography } from "antd";
+
+import React from "react";
+
+const { Title } = Typography;
 import styles from "./CalendarContent.module.scss";
+import { ScheduleOutlined } from "@ant-design/icons";
+import TimelineComponent from "@/components/common/TimeLine";
 
 const CalendarContent: React.FC = () => {
+  interface Item {
+    label: string;
+    children: Item[] | string;
+  }
+
+  const items: Item[] = [
+    {
+      label: "Jan",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "Feb",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "Mar",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "Jan",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "Feb",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "Mar",
+      children: [
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+        {
+          label: "In the 20/21 season",
+          children:
+            "four students from the USACO Grandmaster Class (Tier 5) simultaneously entered the USACO US Camp (USACO Finalist).",
+        },
+      ],
+    },
+    {
+      label: "New Year",
+      children: [],
+    },
+  ];
+
   return (
     <>
       <div className={styles.calendarContent}>
         <div className={`${styles.calendarContainer} container`}>
           <Title className={styles.title}>2023 X-Camp Calendar</Title>
           <div className={styles.listContainer}>
-            adfa 32as1df 32as1df3 5as4d1f
+            <Button className={styles.bookButton}>
+              <ScheduleOutlined />
+            </Button>
+            <TimelineComponent items={items} />
           </div>
         </div>
       </div>
---
+++ src/components/common/TimeLine/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Space, Timeline } from "antd";
+import styles from "./index.module.scss";
+
+interface Item {
+  label: string;
+  children: Item[] | string;
+}
+
+interface Props {
+  items: Item[];
+}
+
+const TimelineComponent: React.FC<Props> = ({ items }) => {
+  const renderTimelineItems = (items: Item[]) => {
+    return items.map((item, index) => (
+      <Timeline.Item key={index} className={styles.timelineItem}>
+        <div className={styles.month}>{item.label}</div>
+        {(item.children as Item[]).map((childrenEvent, index) => (
+          <div key={index}>
+            <Space>
+              <div>
+                <div className={styles.line} />
+              </div>
+              <div className={styles.circle} />
+              <div className={styles.subTitle}>{childrenEvent.label}</div>
+            </Space>
+            {!Array.isArray(childrenEvent.children) && (
+              <div className={styles.subContent}>{childrenEvent.children}</div>
+            )}
+          </div>
+        ))}
+      </Timeline.Item>
+    ));
+  };
+
+  return (
+    <Timeline className={styles.timeline}>
+      {renderTimelineItems(items)}
+    </Timeline>
+  );
+};
+
+export default TimelineComponent;
