34f1750722360b75739bb9d24e759f702e2a58c8
zhangjiajun <183728895@qq.com>
2023-07-10 10:56:44 +0800

    基本完成教育论坛ui

--- src/components/resources/weekly-education-forum/upcoming-events/index.tsx
+++ src/components/resources/weekly-education-forum/upcoming-events/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import styles from "./index.module.scss";
-import { Button, Col, Descriptions, Row } from "antd";
+import React from 'react';
+import styles from './index.module.scss';
+import { Button, Col, Descriptions, Row } from 'antd';
 import {
   ClockCircleOutlined,
   LaptopOutlined,
   RightCircleOutlined,
   UserOutlined,
-} from "@ant-design/icons";
-import ColorfulCard from "@/components/common/colorful-card";
-import XCollapse from "@/components/common/collapse";
+} from '@ant-design/icons';
+import ColorfulCard from '@/components/common/colorful-card';
+import XCollapse from '@/components/common/collapse';
 
 const UpcomingEvents = () => {
   return (
@@ -16,19 +16,19 @@ const UpcomingEvents = () => {
       <div className="container">
         <XCollapse
           header={{
-            title: "Upcoming Events",
+            title: 'Upcoming Events',
             description:
-              "Peek at some alumni events happening just around the corner.",
+              'Peek at some alumni events happening just around the corner.',
           }}
         >
           <Row className={styles.cards} gutter={[32, 32]}>
             {[1, 2, 3]?.map((v, index) => (
               <Col key={index} md={24} lg={8}>
-                <ColorfulCard border={"bottom"} animate={false} index={index}>
+                <ColorfulCard border={'bottom'} animate={false} index={index}>
                   <div className={styles.card}>
                     <div className={styles.date}>
-                      <div className={styles.month}>{"JUN"}</div>
-                      <div className={styles.day}>{"10"}</div>
+                      <div className={styles.month}>{'JUN'}</div>
+                      <div className={styles.day}>{'10'}</div>
                     </div>
                     <div className={styles.title}>
                       {
@@ -42,13 +42,13 @@ const UpcomingEvents = () => {
                     </div>
                     <Descriptions column={1} className={styles.descriptions}>
                       <Descriptions.Item label={<ClockCircleOutlined />}>
-                        {"Tuesday, June 13, 2023 | 6:00 PM - 7:00 PM PDT"}
+                        {'Tuesday, June 13, 2023 | 6:00 PM - 7:00 PM PDT'}
                       </Descriptions.Item>
                       <Descriptions.Item label={<LaptopOutlined />}>
-                        {"Zoom"}
+                        {'Zoom'}
                       </Descriptions.Item>
                       <Descriptions.Item label={<UserOutlined />}>
-                        {"Organizer | X-Camp "}
+                        {'Organizer | X-Camp '}
                       </Descriptions.Item>
                     </Descriptions>
                   </div>
