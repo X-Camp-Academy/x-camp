8c7b2b1b25d9b26ec7122fc2b23314f6e58b50de
chenliangliang <llight105#163.com>
2023-07-11 13:03:44 +0800

    投递简历

--- src/components/about-us/join-us/index.tsx
+++ src/components/about-us/join-us/index.tsx
@@ -1,16 +1,12 @@
 "use client";
 import React from "react";
-import {
-  ConfigProvider,
-  Layout,
-} from "antd";
+import { ConfigProvider, Layout } from "antd";
 import styles from "./index.module.scss";
 import dynamic from "next/dynamic";
 
 const TopBanner = dynamic(() => import("./TopBanner"));
 const JobSelection = dynamic(() => import("./JobSelection"));
 const JoinUsFaculty = dynamic(() => import("./JoinUsFaculty"));
-const WhyWorkWithUs = dynamic(() => import("./WhyWorkWithUs"));
 
 const { Content } = Layout;
 
@@ -28,7 +24,6 @@ const JoinUs: React.FC = () => {
           <TopBanner />
           <JobSelection />
           <JoinUsFaculty />
-          <WhyWorkWithUs/>
         </Content>
       </Layout>
     </ConfigProvider>
