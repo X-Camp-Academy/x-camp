ec720fe6be7970fcd44bbd99dd865605d41f2a5e
= <=>
2023-07-11 11:14:40 +0800

    save

--- src/components/common/nav/define.tsx
+++ src/components/common/nav/define.tsx
@@ -1,56 +1,58 @@
-import Link from 'next/link';
-import { XStarMenuItemType } from './x-star-menu';
-import { useRouter } from 'next/navigation';
-import { CalendarOutlined, UserAddOutlined } from '@ant-design/icons';
+import Link from "next/link";
+import { XStarMenuItemType } from "./x-star-menu";
+import { useRouter } from "next/navigation";
+import { CalendarOutlined, UserAddOutlined } from "@ant-design/icons";
 
 export const useMenuItems = () => {
   const router = useRouter();
   const menuItems: XStarMenuItemType[] = [
     {
       label: <Link href="/">Home</Link>,
-      key: '/',
+      key: "/",
     },
     {
       label: <a>Courses</a>,
-      key: 'courses',
+      key: "courses",
       dropdown: {
         left: {
-          title: 'X-Camp Class Road Map',
+          title: "X-Camp Class Road Map",
           description:
-            '学习路线简介学习路线简介学习路线简介学习路线简介学习路线简介学习路线简介',
+            "学习路线简介学习路线简介学习路线简介学习路线简介学习路线简介学习路线简介",
           btn: (
             <>
-              <span>{'Find My Class'}</span>
+              <span onClick={() => router.push("/courses")}>
+                {"Find My Class"}
+              </span>
               <UserAddOutlined />
             </>
           ),
         },
         right: {
-          title: 'Course Catalog',
-          description: 'List of all courses in X-Camp 2023',
-          action: () => router.push('/courses/catalog'),
+          title: "Course Catalog",
+          description: "List of all courses in X-Camp 2023",
+          action: () => router.push("/courses/catalog"),
         },
       },
       children: [
         {
           label: <Link href="/courses#classify0">Online Classes</Link>,
-          key: '/courses/#classify0',
+          key: "/courses/#classify0",
         },
         {
           label: <Link href="/">100 Prob Challenge</Link>,
-          key: '/courses/100-prob-challenge',
+          key: "/courses/100-prob-challenge",
         },
         {
           label: <Link href="/courses/camps">In-person Campus</Link>,
-          key: '/courses/in-person-campus',
+          key: "/courses/in-person-campus",
         },
         {
           label: <Link href="/courses#classify5">APCS Classes</Link>,
-          key: '/courses/#classify5',
+          key: "/courses/#classify5",
         },
         {
           label: <Link href="/courses#classify3">Enhancement Classes</Link>,
-          key: '/courses/enhancement-classes',
+          key: "/courses/enhancement-classes",
         },
         {
           label: (
@@ -58,21 +60,21 @@ export const useMenuItems = () => {
               X-Tutors
             </a>
           ),
-          key: 'x-tutors',
+          key: "x-tutors",
         },
       ],
     },
     {
       label: <Link href="/">Resources</Link>,
-      key: 'resources',
+      key: "resources",
       dropdown: {
         left: {
-          title: 'X-Camp School Calendar',
+          title: "X-Camp School Calendar",
           description:
-            'X-Camp学校日历展示本年度的开学和结束课程时间,教育论坛和比赛等',
+            "X-Camp学校日历展示本年度的开学和结束课程时间,教育论坛和比赛等",
           btn: (
             <>
-              <span>{'View Calendar'}</span>
+              <span>{"View Calendar"}</span>
               <CalendarOutlined />
             </>
           ),
@@ -85,29 +87,29 @@ export const useMenuItems = () => {
               Weekly Education Forum
             </Link>
           ),
-          key: '/resources/weekly-education-forum/',
+          key: "/resources/weekly-education-forum/",
         },
         {
           label: <Link href="/">Turing cup</Link>,
-          key: '/resources/turing-cup/',
+          key: "/resources/turing-cup/",
         },
         {
           label: <Link href="/">USACO</Link>,
-          key: '/resources/usaco/',
+          key: "/resources/usaco/",
         },
         {
           label: (
             <Link href="/resources/weekly-open-house">Weekly Open House</Link>
           ),
-          key: '/resources/weekly-open-house/',
+          key: "/resources/weekly-open-house/",
         },
         {
           label: <Link href="/">X-Cup</Link>,
-          key: '/resources/x-cup/',
+          key: "/resources/x-cup/",
         },
         {
           label: <Link href="/">ACSL</Link>,
-          key: '/resources/acsl/',
+          key: "/resources/acsl/",
         },
         {
           label: (
@@ -115,42 +117,42 @@ export const useMenuItems = () => {
               USACo Live solution
             </Link>
           ),
-          key: '/resources/usaco-live-solution/',
+          key: "/resources/usaco-live-solution/",
         },
         {
           label: <Link href="/">Teamscode</Link>,
-          key: 'teamscode',
+          key: "teamscode",
         },
         {
           label: <Link href="/">Calico</Link>,
-          key: 'calico',
+          key: "calico",
         },
         {
           label: <Link href="/resources/contests">Contests</Link>,
-          key: '/resources/contests/',
+          key: "/resources/contests/",
         },
 
         {
           label: <Link href="/">Art of Programming</Link>,
-          key: 'art-of-programming',
+          key: "art-of-programming",
         },
         {
           label: <Link href="/">Proco</Link>,
-          key: 'proco',
+          key: "proco",
         },
       ],
     },
     {
-      label: <Link href="/">About Us</Link>,
-      key: 'about-us',
+      label: <Link href="/about-us/introduction">About Us</Link>,
+      key: "about-us",
       dropdown: {
         left: {
-          title: 'X-Camp School Calendar',
+          title: "X-Camp School Calendar",
           description:
-            'X-Camp学校日历展示本年度的开学和结束课程时间,教育论坛和比赛等',
+            "X-Camp学校日历展示本年度的开学和结束课程时间,教育论坛和比赛等",
           btn: (
             <>
-              <span>{'View Calendar'}</span>
+              <span>{"View Calendar"}</span>
               <CalendarOutlined />
             </>
           ),
@@ -159,43 +161,43 @@ export const useMenuItems = () => {
       children: [
         {
           label: <Link href="/about-us/introduction">Introduction</Link>,
-          key: 'introduction',
+          key: "introduction",
         },
         {
           label: <Link href="/about-us/calendar">School Calendar</Link>,
-          key: 'school-calendar',
+          key: "school-calendar",
         },
         {
           label: <Link href="/about-us/contact-us">Contact Us</Link>,
-          key: 'contact-us',
+          key: "contact-us",
         },
         {
           label: <Link href="/about-us/achievements">Achievements</Link>,
-          key: 'achievements',
+          key: "achievements",
         },
         {
           label: <Link href="/about-us/introduction">Faculty&Coach</Link>,
-          key: 'faculty&coach',
+          key: "faculty&coach",
         },
         {
           label: <Link href="/about-us/join-us">Join Us</Link>,
-          key: 'join-us',
+          key: "join-us",
         },
         {
           label: <Link href="/about-us/partners">Partners</Link>,
-          key: 'partners',
+          key: "partners",
         },
         {
           label: <Link href="/about-us/news">News</Link>,
-          key: 'news',
+          key: "news",
         },
         {
           label: <Link href="/about-us/help-center">QA</Link>,
-          key: 'help-center',
+          key: "help-center",
         },
         {
           label: <Link href="/about-us/x-alumni">X-Alumni</Link>,
-          key: 'x-alumni',
+          key: "x-alumni",
         },
         {
           label: (
@@ -203,15 +205,14 @@ export const useMenuItems = () => {
               Current Student Referral Program
             </Link>
           ),
-          key: 'current-student-referral-program',
+          key: "current-student-referral-program",
         },
       ],
     },
     {
       label: <Link href="/">Evaluation</Link>,
-      key: 'evaluation',
+      key: "evaluation",
     },
-    
   ];
   return menuItems;
 };
