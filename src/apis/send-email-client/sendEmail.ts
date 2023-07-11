3527945c56248653e89729daef33e27eeb2fa688
Tangent <605738695@qq.com>
2023-07-11 10:26:30 +0800

    design api

--- src/apis/send-email-client/sendEmail.ts
+++ src/apis/send-email-client/sendEmail.ts
@@ -1,6 +1,10 @@
 import { useRequest } from "ahooks";
 import { message } from "antd";
-import { openClassEmailRequest, useSendEmailClient } from ".";
+import {
+  openClassEmailRequest,
+  subscribeNewsletterRequest,
+  useSendEmailClient,
+} from ".";
 
 export const useSendOpenClassEmail = () => {
   const client = useSendEmailClient();
@@ -15,6 +19,55 @@ export const useSendOpenClassEmail = () => {
         message.success({
           key: "sendEmailSuccessfully",
           content: "已将公开课信息发送至您的邮箱，请注意查收！",
+        });
+      },
+      onError: (error: any) => {
+        console.log(error);
+      },
+    }
+  );
+};
+
+export const useSubscribeNewsletter = () => {
+  const client = useSendEmailClient();
+  return useRequest(
+    async (params: subscribeNewsletterRequest) => {
+      const resp = await client.subscribeNewsletter(params);
+      return resp;
+    },
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success({
+          key: "sendEmailSuccessfully",
+          content: "订阅成功，请查收您的邮箱",
+        });
+      },
+      onError: (error: any) => {
+        console.log(error);
+      },
+    }
+  );
+};
+
+/**
+ * 上传用户简历
+ * @param Client
+ * @returns
+ */
+export const useSubmitResume = () => {
+  const client = useSendEmailClient();
+  return useRequest(
+    async (params: FormData) => {
+      const resp = await client.submitResume(params);
+      return resp;
+    },
+    {
+      manual: true,
+      onSuccess: () => {
+        message.success({
+          key: "sendEmailSuccessfully",
+          content: "已将简历发送至X-Camp邮箱",
         });
       },
       onError: (error: any) => {
