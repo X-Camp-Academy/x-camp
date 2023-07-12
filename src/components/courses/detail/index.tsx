de0ebca8e46f967ef016cfb8d50b0e53044d5f5d
金子扬 <1528249383@qq.com>
2023-07-12 10:28:58 +0800

    完成课程详情页面的修改

--- src/components/courses/detail/index.tsx
+++ src/components/courses/detail/index.tsx
@@ -1,15 +1,15 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./index.module.scss";
 import { ConfigProvider, Layout } from "antd";
 import Testimony from "@/components/home/Testimony";
 import TopBanner from "./top-banner";
-import UsacoMedal from "@/components/about-us/introduction/UsacoMedal";
+import UsacoMedal from "@/components/common/usaco-cards";
 import FacultyCoach from "@/components/about-us/introduction/FacultyCoach";
 import ProgressionClasses from "./progression-classes";
 import { useGetClasses, useGetCourses } from "@/apis/strapi-client/strapi";
 import { useGetFaq, useGetTestimony } from "@/apis/strapi-client/strapi";
-import { FaqCategory } from "@/apis/strapi-client/define";
+import { FaqCategory, GetFaq } from "@/apis/strapi-client/define";
 import Faqs from "@/components/common/faqs";
 import { useParams } from "next/navigation";
 import CourseClassesContext from "./CourseClasses";
@@ -17,7 +17,6 @@ const { Content } = Layout;
 
 const CourseDetail = () => {
   const params = useParams();
-
   // 请求当前 courseId 的评论
   const { data: testimonyData } = useGetTestimony({
     ready: true,
@@ -27,16 +26,17 @@ const CourseDetail = () => {
   const { data: coursesData } = useGetCourses({
     id: { $eq: Number(params?.courseId) },
   });
-  // console.log(coursesData);
 
   const { data } = useGetClasses();
-  // console.log(data);
+
 
   const { data: faq } = useGetFaq({
     ready: true,
     category: FaqCategory.CoursesQA,
+    courseId: [params?.courseId],
   });
-  
+
+
   // StrapiResponseDataItem<GetCourses>
 
   return (
@@ -54,7 +54,12 @@ const CourseDetail = () => {
           >
             <TopBanner />
           </CourseClassesContext.Provider>
-          <UsacoMedal />
+          <div className="container" style={{
+            marginTop: 100
+          }}>
+            <UsacoMedal showTitle={true} />
+          </div>
+
           <ProgressionClasses />
           <FacultyCoach />
           <Faqs title="Course FAQs" data={faq} />
