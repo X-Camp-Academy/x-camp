bd77e7ff93d8f3788d582b913d3080f769dcc5da
healthyhcc <healthyhcc@gmail.com>
2023-07-18 14:27:54 +0800

    修改推荐课程

--- src/components/courses/detail/top-banner/course-card/course-description/index.tsx
+++ src/components/courses/detail/top-banner/course-card/course-description/index.tsx
@@ -194,49 +194,6 @@ const CourseDescription = () => {
           </Space>
         </div>
       </div>
-      <div className={styles.right}>
-        <Descriptions column={1}>
-          <Descriptions.Item label={t("ClassMode")}>
-            {classMode}
-          </Descriptions.Item>
-          <Descriptions.Item label={t("CodeLanguage")}>
-            {classLang}
-          </Descriptions.Item>
-          <Descriptions.Item label={t("ClassroomLanguage")}>
-            {classRoomLang}
-          </Descriptions.Item>
-          <Descriptions.Item label={t("Duration")}>
-            {`${formatDate(startDate || "")} - ${formatDate(endDate || "")}`}
-          </Descriptions.Item>
-          <Descriptions.Item label={t("CourseFormat")}>
-            {courseFormat}
-          </Descriptions.Item>
-        </Descriptions>
-        <Button
-          id="copyButton"
-          className={styles.btn}
-          onClick={() => {
-            handlerShareLesson();
-          }}
-        >
-          {t("ShareLessons")}
-          <ShareAltOutlined />
-        </Button>
-        <Modal
-          title={getTransResult(lang, "分享课程信息", 'Share course information')}
-          open={isModalOpen}
-          onOk={handleOk}
-          onCancel={handleCancel}
-          closeIcon={false}
-          footer={[
-            <Button key="submit" type="primary" onClick={handleOk}>
-              {getTransResult(lang, '复制课程信息', 'Copy the course information')}
-            </Button>
-          ]}
-        >
-          <div style={{ whiteSpace: 'pre-line' }}>{getTransResult(lang, clipTextZh, clipTextEn)}</div>
-        </Modal>
-      </div>
     </Space>
   );
 };
