bf93cb75d5f9c4dbc2f827d1a5ff1744fa91086c
hcc <healthyhcc@gmail.com>
2023-09-22 16:31:11 +0800

    feat: 自定义各个页面元数据信息配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { generateMetadata } from '@/utils/metadata';
 import dynamic from 'next/dynamic';
+
 const Home = dynamic(() => import('@/components/home'));
 
 export const metadata = {
-  title: 'X-Camp Academy',
-  description: 'X-Camp Academy | Programming and Education news from X-Camp'
+  ...generateMetadata('home')
 };
 
 const Page: React.FC = () => {
