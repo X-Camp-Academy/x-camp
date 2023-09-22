071511c031d15a3170eb922df418eab1c104d080
hcc <healthyhcc@gmail.com>
2023-09-22 17:51:17 +0800

    feat: serve fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,16 @@ export const metadata = {
   ...generateMetadata('home')
 };
 
+async function getData() {
+  const res = await fetch('https://strapi.turingstar.com.cn/api/xc-course-level-types');
+
+  return res.json();
+}
+
 const Page: React.FC = () => {
+  const data = getData();
+  console.log(data);
+
   return <Home />;
 };
 
