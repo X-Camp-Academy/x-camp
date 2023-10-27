aba98721e1cd62c543ec8259a16d3d6d2fc5ce3e
hcc <healthyhcc@gmail.com>
2023-10-27 11:08:30 +0800

    fix: fix mobile title bg

--- src/components/common/common-banner/index.tsx
+++ src/components/common/common-banner/index.tsx
@@ -1,3 +1,4 @@
+import { useMobile } from '@/utils';
 import { Col, Row, Space, Typography } from 'antd';
 import React from 'react';
 import styles from './index.module.scss';
@@ -15,16 +16,17 @@ interface CommonBannerProps {
 }
 
 const Banner: React.FC<CommonBannerProps> = ({ title, paragraph, image, titleClassName, paragraphClassName, barColor, backgroundColor }) => {
+  const isMobile = useMobile();
   return (
     <Row className={styles.row}>
-      <Col xs={24} sm={24} md={24} lg={24} xl={10}>
-        <Space direction="vertical" className={styles.leftSpace} style={{ backgroundColor }}>
+      <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+        <Space direction="vertical" size={isMobile ? 0 : 16} className={styles.leftSpace} style={{ backgroundColor }}>
           <Title className={`${titleClassName || styles.title}`}>{title}</Title>
           <Paragraph className={`${paragraphClassName || styles.paragraph}`}>{paragraph}</Paragraph>
         </Space>
         <div className={styles.background} style={{ backgroundColor: barColor }} />
       </Col>
-      <Col xs={24} sm={24} md={24} lg={24} xl={14}>
+      <Col xs={24} sm={24} md={24} lg={24} xl={16}>
         <img alt="img" src={image} className={styles.image} />
       </Col>
     </Row>
