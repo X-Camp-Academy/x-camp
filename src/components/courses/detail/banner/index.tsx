286042cbb2c4513a248e0f524377632c371c88c5
hcc <healthyhcc@gmail.com>
2023-10-29 17:42:49 +0800

    课程详情页移动端调整

--- src/components/courses/detail/banner/index.tsx
+++ src/components/courses/detail/banner/index.tsx
@@ -21,7 +21,7 @@ const CourseBanner: React.FC = () => {
   });
   const [isModalOpen, setIsModalOpen] = useState(false);
   const courseData = useContext(CourseClassesContext);
-
+  const defaultMedia = 'https://media.strapi.turingstar.com.cn/production/2023/7/20230726_162259_bac67c1a78.mp4?autoplay=0';
   const { courseCode, classMode, classLang, spokenLang, startDateTime, endDateTime, media, courseTitleZh, courseTitleEn, lessonNum, frequency, courseFormat, additionalInfo } =
     courseData?.attributes ?? {};
 
@@ -117,20 +117,29 @@ const CourseBanner: React.FC = () => {
 
               <div className={styles.right}>
                 <Carousel dots={false} ref={ref}>
-                  {media?.data?.map((mediaItem) => {
-                    return imageMimes?.includes(mediaItem?.attributes?.mime) ? (
-                      <div key={mediaItem?.id} className={styles.videoBox}>
-                        <Image alt="" preview={false} src={mediaItem?.attributes?.url} width="100%" height="100%" />
-                      </div>
-                    ) : (
-                      <div key={mediaItem?.id} className={styles.videoBox}>
-                        <video controls>
-                          <source src={mediaItem?.attributes?.url} type="video/mp4" />
-                          {t('VideoProblem')}
-                        </video>
-                      </div>
-                    );
-                  })}
+                  {media?.data && media?.data?.length > 0 ? (
+                    media?.data?.map((mediaItem) => {
+                      return imageMimes?.includes(mediaItem?.attributes?.mime) ? (
+                        <div key={mediaItem?.id} className={styles.videoBox}>
+                          <Image alt="" preview={false} src={mediaItem?.attributes?.url} width="100%" height="100%" />
+                        </div>
+                      ) : (
+                        <div key={mediaItem?.id} className={styles.videoBox}>
+                          <video controls>
+                            <source src={mediaItem?.attributes?.url} type="video/mp4" />
+                            {t('VideoProblem')}
+                          </video>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <div className={styles.videoBox}>
+                      <video controls>
+                        <source src={defaultMedia} type="video/mp4" />
+                        {t('VideoProblem')}
+                      </video>
+                    </div>
+                  )}
                 </Carousel>
 
                 <div className={styles.mediaChoice}>
--- src/components/courses/detail/syllabus/index.tsx
+++ src/components/courses/detail/syllabus/index.tsx
@@ -1,5 +1,4 @@
 import { useLang } from '@/hoc/with-intl/define';
-import { useMobile } from '@/utils';
 import { getLangResult } from '@/utils/public';
 import { Space, Typography } from 'antd';
 import React, { useContext } from 'react';
@@ -9,25 +8,28 @@ import styles from './index.module.scss';
 const { Title, Text } = Typography;
 
 const CourseSyllabus: React.FC = () => {
-  const isMobile = useMobile();
   const { format: t, lang } = useLang();
   const courseData = useContext(CourseClassesContext);
   const courseSyllabus = getLangResult(lang, courseData?.attributes?.courseSyllabusZh, courseData?.attributes?.courseSyllabusEn);
 
   return (
-    <div className={styles.content}>
-      <div className="container">
-        <Title className={styles.title}>{t('TopicsCovered')}</Title>
-        <Space size={24} direction="vertical" className={styles.syllabus}>
-          {courseSyllabus?.map((item, index) => (
-            <Space key={item} align="center">
-              <Text className={styles.text}>{index + 1}</Text>
-              <Text className={styles.paragraph}>{item}</Text>
+    <>
+      {courseSyllabus && courseSyllabus?.length > 0 && (
+        <div className={styles.content}>
+          <div className="container">
+            <Title className={styles.title}>{t('TopicsCovered')}</Title>
+            <Space size={24} direction="vertical" className={styles.syllabus}>
+              {courseSyllabus?.map((item, index) => (
+                <Space key={item} align="center">
+                  <Text className={styles.text}>{index + 1}</Text>
+                  <Text className={styles.paragraph}>{item}</Text>
+                </Space>
+              ))}
             </Space>
-          ))}
-        </Space>
-      </div>
-    </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
