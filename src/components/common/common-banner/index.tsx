29b03f5780608ae488490da48e595bebc6b546e8
jzyhahahah <1528249383@qq.com>
2023-10-31 09:42:49 +0800

    fix: solving banner problem

--- src/components/common/common-banner/index.tsx
+++ src/components/common/common-banner/index.tsx
@@ -1,6 +1,6 @@
 import { useMobile } from '@/utils';
-import { Col, Row, Space, Typography } from 'antd';
-import React from 'react';
+import { Space, Typography } from 'antd';
+import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss';
 
 const { Title, Paragraph } = Typography;
@@ -21,6 +21,23 @@ interface CommonBannerProps {
 
 const Banner: React.FC<CommonBannerProps> = ({ title, paragraph, image, titleClassName, paragraphClassName, barColor, backgroundColor, titleStyle, paragraphStyle, time, paragraphFontSize }) => {
   const isMobile = useMobile();
+  const [bgImgRight, setBgImgRight] = useState('100%');
+
+  useEffect(() => {
+    if (window.innerWidth >= 1400) {
+      setBgImgRight('0');
+    } else if (window.innerWidth >= 1200) {
+      setBgImgRight('-10%');
+    } else if (window.innerWidth >= 992) {
+      setBgImgRight('-15%');
+    } else if (window.innerWidth >= 768) {
+      setBgImgRight('-20%');
+    } else if (window.innerWidth >= 576) {
+      setBgImgRight('-25%');
+    } else {
+      setBgImgRight('-30%');
+    }
+  }, [window.innerWidth]);
   return (
     <>
       {isMobile ? (
@@ -42,8 +59,35 @@ const Banner: React.FC<CommonBannerProps> = ({ title, paragraph, image, titleCla
           </Space>
         </div>
       ) : (
-        <Row className={styles.row}>
-          <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+        /*         <Row className={styles.row}>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={8}>
+                    <Space direction="vertical" size={16} className={styles.leftSpace} style={{ backgroundColor }}>
+                      <Title className={`${titleClassName || styles.title}`}>{title}</Title>
+                      {time && <div className={styles.time}>{time}</div>}
+                      <Paragraph
+                        className={`${paragraphClassName || styles.paragraph}`}
+                        style={{
+                          fontSize: paragraphFontSize ? paragraphFontSize : '24px'
+                        }}
+                      >
+                        {paragraph}
+                      </Paragraph>
+                    </Space>
+                    <div className={styles.background} style={{ backgroundColor: barColor }} />
+                  </Col>
+                  <Col xs={24} sm={24} md={24} lg={24} xl={16}>
+                    <img alt="img" src={image} className={styles.image} />
+                  </Col>
+                </Row> */
+        <div
+          className={styles.bannerContainer}
+          style={{
+            background: `url('${image}') no-repeat`,
+            // eslint-disable-next-line quotes
+            backgroundPosition: `top 0 right ${bgImgRight}`
+          }}
+        >
+          <div className={`${styles.row} container`}>
             <Space direction="vertical" size={16} className={styles.leftSpace} style={{ backgroundColor }}>
               <Title className={`${titleClassName || styles.title}`}>{title}</Title>
               {time && <div className={styles.time}>{time}</div>}
@@ -56,12 +100,9 @@ const Banner: React.FC<CommonBannerProps> = ({ title, paragraph, image, titleCla
                 {paragraph}
               </Paragraph>
             </Space>
-            <div className={styles.background} style={{ backgroundColor: barColor }} />
-          </Col>
-          <Col xs={24} sm={24} md={24} lg={24} xl={16}>
-            <img alt="img" src={image} className={styles.image} />
-          </Col>
-        </Row>
+          </div>
+          {/* <div className={styles.background} style={{ backgroundColor: barColor }} /> */}
+        </div>
       )}
     </>
   );
