1bc4850ea6a08697d502cbd81fba99980ab1c96a
hcc <healthyhcc@gmail.com>
2023-11-22 22:55:56 -0800

    update xuyuan's reviews

--- src/components/about-us/join-us/submit-resume/banner/index.tsx
+++ src/components/about-us/join-us/submit-resume/banner/index.tsx
@@ -2,7 +2,6 @@ import CommonBanner from '@/components/common/common-banner';
 import { useLang } from '@/hoc/with-intl/define';
 import { CommentOutlined } from '@ant-design/icons';
 import React from 'react';
-import styles from './index.module.scss';
 
 const Banner: React.FC = () => {
   const { format: t } = useLang();
@@ -13,17 +12,15 @@ const Banner: React.FC = () => {
     </>
   );
   return (
-    <div className={styles.topBannerContainer}>
-      <CommonBanner
-        image={'/image/about-us/banner-submit-resume.png'}
-        title={t('Careers')}
-        paragraph={paragraph}
-        showButton
-        buttonIcon={<CommentOutlined />}
-        buttonText={t('ContactUs')}
-        buttonLink={'/about-us/contact-us'}
-      />
-    </div>
+    <CommonBanner
+      image={'/image/about-us/banner-submit-resume.png'}
+      title={t('Careers')}
+      paragraph={paragraph}
+      showButton
+      buttonIcon={<CommentOutlined />}
+      buttonText={t('ContactUs')}
+      buttonLink={'/about-us/contact-us'}
+    />
   );
 };
 
