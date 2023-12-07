5ca62ac406ce78f1792fdf782b0449da4776ae1d
hcc <healthyhcc@gmail.com>
2023-12-07 16:10:22 +0800

    fix google tracking

--- src/components/common/google-tracking/index.tsx
+++ src/components/common/google-tracking/index.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/next-script-for-ga */
-import Head from 'next/head';
 
 const GoogleTracking = () => {
   return (
     <>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11428221684" />
-      <Head>
+      {/* <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11428221684" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -16,7 +15,7 @@ const GoogleTracking = () => {
             `
           }}
         />
-      </Head>
+      </Head> */}
     </>
   );
 };
