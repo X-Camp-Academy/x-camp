797d124cdb7ac83b275f30fb0acc51bd26d5ee6c
cwt <18806850924@163.com>
2024-01-23 15:08:34 +0800

    test: commit test

--- src/hooks/useWebUpdateNotify.tsx
+++ src/hooks/useWebUpdateNotify.tsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useRef, useState} from "react";
-import {Button, notification} from 'antd'
+import { Button, notification } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 
 enum LangType {
   'zh_CN' = 'zh_CN',
@@ -7,39 +7,39 @@ enum LangType {
 }
 
 const notifyLocaleMap: {
-  [key in LangType]: any
+  [key in LangType]: any;
 } = {
   [LangType.en_US]: {
     title: '📢 system update',
     description: 'System update, please refresh the page',
     buttonText: 'refresh',
-    dismissButtonText: 'dismiss',
+    dismissButtonText: 'dismiss'
   },
   [LangType.zh_CN]: {
     title: '📢 发现新版本',
     description: '网页更新了！请刷新页面后使用。',
     buttonText: '刷新',
-    dismissButtonText: '忽略',
-  },
-}
+    dismissButtonText: '忽略'
+  }
+};
 
-const VERSION_KEY = 'local-version'
+const VERSION_KEY = 'local-version';
 
 const getLocalVersion = (): string => {
-  const res = window.localStorage.getItem(VERSION_KEY)
-  return res ? JSON.parse(res) : ''
-}
+  const res = window.localStorage.getItem(VERSION_KEY);
+  return res ? JSON.parse(res) : '';
+};
 
 const setVersion = (version: any) => {
-  return window.localStorage.setItem(VERSION_KEY, JSON.stringify(version))
-}
+  return window.localStorage.setItem(VERSION_KEY, JSON.stringify(version));
+};
 
 export const useWebUpdateNotify = () => {
-  const [lang, setLang] = useState<'zh_CN' | 'en_US'>(LangType.zh_CN)
+  const [lang, setLang] = useState<'zh_CN' | 'en_US'>(LangType.zh_CN);
   const [api, contextHolder] = notification.useNotification();
-  const [notifyVisible, setNotifyVisible] = useState(false)
+  const [notifyVisible, setNotifyVisible] = useState(false);
 
-  const timer = useRef<any>()
+  const timer = useRef<any>();
 
   const openNotification = () => {
     api.open({
@@ -48,94 +48,109 @@ export const useWebUpdateNotify = () => {
       duration: null,
       style: {
         width: 280,
-        padding: "8px 16px",
-        lineHeight: "1.6"
+        padding: '8px 16px',
+        lineHeight: '1.6'
       },
-      description: <div>
-        <div>{notifyLocaleMap[lang].description}</div>
-        <div style={{textAlign: 'right'}}>
-          <Button style={{color: "rgba(0, 0, 0, 0.25)"}} onClick={() => {
-            setNotifyVisible(false)
-            api.destroy()
-          }} type={"link"}>{notifyLocaleMap[lang].dismissButtonText}</Button>
-          <Button type={"link"} onClick={() => {
-            setNotifyVisible(false)
-            window.location.reload()
-          }}>{notifyLocaleMap[lang].buttonText}</Button>
+      description: (
+        <div>
+          <div>{notifyLocaleMap[lang].description}</div>
+          <div style={{ textAlign: 'right' }}>
+            <Button
+              style={{ color: 'rgba(0, 0, 0, 0.25)' }}
+              onClick={() => {
+                setNotifyVisible(false);
+                api.destroy();
+              }}
+              type={'link'}
+            >
+              {notifyLocaleMap[lang].dismissButtonText}
+            </Button>
+            <Button
+              type={'link'}
+              onClick={() => {
+                setNotifyVisible(false);
+                window.location.reload();
+              }}
+            >
+              {notifyLocaleMap[lang].buttonText}
+            </Button>
+          </div>
         </div>
-      </div>,
+      )
     });
   };
 
   const checkUpdate = async () => {
-    const localVersion = getLocalVersion()
-    console.log(localVersion, 'before')
-    fetch(`/version.json?t=${+new Date()}`).then(res => {
-      return res.json()
-    }).then(({version}) => {
-      console.log('fetched', localVersion, version, localVersion === version)
-      if (version && version !== localVersion) {
-        setVersion(version)
-        localVersion && notify()
-      }
-    }).catch(err => console.warn(err, '----version.json error----'))
-  }
+    const localVersion = getLocalVersion();
+    console.log(localVersion, '--before--');
+    fetch(`/version.json?t=${+new Date()}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then(({ version }) => {
+        console.log('--fetched--', localVersion, version, localVersion === version);
+        if (version && version !== localVersion) {
+          setVersion(version);
+          localVersion && notify();
+        }
+      })
+      .catch((err) => console.warn(err, '----version.json error----'));
+  };
 
   const pollingCheck = () => {
-    timer.current = setInterval(checkUpdate, 10 * 6 * 10 * 1000)
-  }
+    timer.current = setInterval(checkUpdate, 10 * 6 * 10 * 1000);
+  };
 
   const notify = () => {
     if (document.querySelector('.ant-notification')) {
-      return
+      return;
     }
     setNotifyVisible((oldVal) => {
       if (!oldVal) {
-        openNotification()
+        openNotification();
       }
-      return true
-    })
-  }
+      return true;
+    });
+  };
   const RegisterEventListeners = () => {
-    window.addEventListener('focus', checkUpdate)
+    window.addEventListener('focus', checkUpdate);
     window.addEventListener('visibilitychange', () => {
       if (document.visibilityState === 'visible') {
-        pollingCheck()
-        checkUpdate()
+        pollingCheck();
+        checkUpdate();
       }
       if (document.visibilityState === 'hidden') {
-        timer.current && clearInterval(timer.current)
+        timer.current && clearInterval(timer.current);
       }
-    })
+    });
     window.addEventListener(
       'error',
       (err) => {
-        const errTagName = (err?.target as any)?.tagName
-        if (errTagName === 'SCRIPT')
-          checkUpdate()
+        const errTagName = (err?.target as any)?.tagName;
+        if (errTagName === 'SCRIPT') checkUpdate();
       },
-      true,
-    )
-  }
+      true
+    );
+  };
 
   const removeEventListeners = () => {
-    window.removeEventListener('focus', checkUpdate)
-  }
+    window.removeEventListener('focus', checkUpdate);
+  };
 
   useEffect(() => {
     if (window) {
-      RegisterEventListeners()
-      pollingCheck()
+      RegisterEventListeners();
+      pollingCheck();
     }
 
     return () => {
-      removeEventListeners()
-    }
-  }, [])
+      removeEventListeners();
+    };
+  }, []);
 
   return {
     setLocale: setLang,
     contextHolder,
     checkUpdate
-  }
-}
+  };
+};
